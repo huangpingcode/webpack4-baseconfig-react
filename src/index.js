a65ffef60664ae3372564dd0d46ee16030406746
huang-hping <652463887@qq.com>
2018-08-15 22:38:52 +0800

    backup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 
 import app from "./app/app"
 import login from "./login/login"
+import React from "react"
+import jquery from "jquery"
+console.log("React", React)
+console.log("jquery", jquery)
 console.log(app, "app")
 console.log(login, "appfd")
--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,106 @@
 const path = require('path')
-const applyMockMiddleware = require("./applyMockMiddleware")
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+const applyMockMiddleware = require("./applyMockMiddleware")
 // console.log("process.env.NODE_ENV", process.env)
 
-let webpackBaseConfig = {
-    // mode: 'production', // development
+// setTimeout(() => {
+//     process.exit(0)
+// }, 5000);
+// let fs = require("fs")
+module.exports = function (env, argv) {
+  let isDev = !env || !env.production || argv.mode === "development"
+  let webpackBaseConfig = {
     // publicPath: '',
+    devtool: isDev ? "" : "",
     entry: {
-        app: './src/index'
-        // common: ["./src/common/util.js", "./src/common/constant.js", "./src/common/eventEmitter.js",
-        //     "./src/server/chatSdk.js"
-        // ]
+      app: './src/index'
     },
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: './[name]-[hash:8].js',
+      path: path.resolve(__dirname, "dist"),
+      filename: './[name]-[hash:8].js',
     },
     module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }, {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", 'postcss-loader']
-            }, {
-                test: /\.less$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader", 'postcss-loader']
-            },
-            {
-                test: /\.(jpg|png)$/,
-                use: ['url-loader?limit=8192&name=img/[hash:8].[ext]']
-            }
-            // }, {
-            //     test: /\.(eot|svg|ttf|woff)$/,
-            //     use: ['file-loader?name=lib/font/[hash:8].[ext]']
-            // }
-        ]
+      rules: [
+        {
+          test: /\.jsx?$/, exclude: /node_modules/, use: ["babel-loader"]
+        }, {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", 'postcss-loader']
+        }, {
+          test: /\.less$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader", 'postcss-loader']
+        },
+        {
+          test: /\.(jpg|png)$/, use: ['url-loader?limit=8192&name=[hash:8].[ext]']
+        },
+        // { test: /\.(eot|svg|ttf|woff)$/, use: ['file-loader?name=lib/font/[hash:8].[ext]'] }
+      ]
     },
     // optimization: {
     //     // minimizer: true,    // 压缩
     // },
     resolve: {
-        extensions: ['.js', '.jsx', '.json']        // 自动补全文件后缀
+      extensions: ['.js', '.jsx', '.json'],        // 自动补全文件后缀
+      alias: {
+        "src": "./src"
+      }
+    },
+    externals: {
+      'jquery': 'window.jQuery',
+      "react": 'window.React'
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new MiniCssExtractPlugin({
-            filename: "[name].[chunkhash:8].css",
-            chunkFilename: "[id].css"
-        })
+      new CleanWebpackPlugin(['dist']),
+      new MiniCssExtractPlugin({
+        filename: "[name].[chunkhash:8].css",
+        chunkFilename: "[id].css"
+      }),
+      new HtmlWebpackPlugin({
+        title: "webpack 4 基本测试",
+        filename: 'index.html',
+        template: './src/html/template.ejs',
+        inject: 'body',
+        // chunks: ['index', 'main'],
+        // excludeChunks: ['devor.js'],
+        // favicon: 'path/to/favicon.ico',
+        minify: isDev ? null : { //压缩HTML文件
+          removeComments: true, //移除HTML中的注释
+          collapseWhitespace: true, //删除空白符与换行符
+          minifyJS: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true
+        }
+      })
     ]
-}
-// setTimeout(() => {
-//     process.exit(0)
-// }, 5000);
-let fs = require("fs")
-module.exports = function (env, argv) {
-    let webpackConfig = {}
-    if ((env && env.production) || argv.mode === "production") {
-        webpackConfig = Object.assign(webpackConfig, webpackBaseConfig, {
+  }
+  let webpackConfig = {}
+  if ((env && env.production) || argv.mode === "production") {
+    webpackConfig = Object.assign(webpackConfig, webpackBaseConfig, {
 
-        })
-    }
-    else {
-        webpackConfig = Object.assign(webpackConfig, webpackBaseConfig, {
-            devtool: 'source-map',
-            devServer: {
-                host: '0.0.0.0',
-                port: '3000',
-                hot: true,
-                // contentBase: "",
-                before: function (app) {
-                    applyMockMiddleware(app)
-                },
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                }
-            }
-        })
-    }
-
-    return new Promise((resolve, reject) => {
-        resolve(webpackConfig)
     })
+  }
+  else {
+    webpackConfig = Object.assign(webpackConfig, webpackBaseConfig, {
+
+      devServer: {
+        host: '0.0.0.0',
+        port: '3000',
+        hot: true,
+        // contentBase: "",
+        before: function (app) {
+          applyMockMiddleware(app)
+        },
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      }
+    })
+  }
+
+  return new Promise((resolve, reject) => {
+    resolve(webpackConfig)
+  })
 } 
